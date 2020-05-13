9bb87a93b00bff4972b0e9c1b6ea425dba6793a5
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2020-05-13 08:21:47 +0300

    Stuffasd

---
+++ .config/configuration.nix
@@ -0,0 +1,216 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  ########################################
+
+  # Allow unfree
+  nixpkgs.config.allowUnfree = true;
+  ########################################
+
+  # Linux kernel
+  boot.kernelPackages = pkgs.linuxPackages_5_6;
+  security.allowSimultaneousMultithreading = false;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [ "mitigations=off" ];
+  ########################################
+
+  networking.hostName = "yusuf-pc"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp6s0.useDHCP = true;
+  ########################################
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "tr_TR.UTF-8";
+  console = {
+    font = "7x14";
+    keyMap = "trq";
+  };
+  time.timeZone = "Turkey";
+  ########################################
+
+  # caching dns server
+  services.dnsmasq = {
+    enable = true;
+    servers = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
+   };
+
+  services.earlyoom.enable = true;
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemuPackage = pkgs.qemu_kvm;
+  };
+  
+  ########################################
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  # 32bit for steam
+  hardware.pulseaudio.support32Bit = true;
+
+  # OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    extraPackages = with pkgs; [ libvdpau-va-gl vaapiVdpau libva ];
+    # 32bit for steam
+    driSupport32Bit = true;
+    extraPackages32 = with pkgs.pkgsi686Linux; [ libvdpau-va-gl vaapiVdpau libva ];
+  };
+  ########################################
+
+  # Enable xorg
+  #services.xserver.enable = true;
+  
+  # Do NOT enable xdg.portal, causes gnome to freeze at startup for a while.
+  #services.xserver.desktopManager.gnome3.enable = true;
+  #environment.gnome3.excludePackages = with pkgs.gnome3; [
+  #  cheese empathy evolution gnome-maps gnome-music gnome-online-accounts gnome-software
+  #  gnome-photos gnome-contacts gnome-calendar gnome-characters gnome-clocks gnome-bluetooth
+  #  folks epiphany geary totem gnome-weather gnome-terminal
+  #];
+  #services.xserver.displayManager.gdm = {
+  #  enable = true;
+  #  autoLogin = {
+  #    enable = true;
+  #    user = "yusuf";
+  #  };
+  #};
+
+  # sway
+  programs.sway = {
+    enable = true;
+    extraPackages = with pkgs; [
+        swaylock swayidle xwayland rofi alacritty wl-clipboard grim imv
+    ];
+    extraSessionCommands = ''
+        export SDL_VIDEODRIVER=wayland
+        # needs qt5.qtwayland in systemPackages
+        export QT_QPA_PLATFORM=wayland
+        export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+        # Fix for some Java AWT applications (e.g. Android Studio),
+        # use this if they aren't displayed properly:
+        export _JAVA_AWT_WM_NONREPARENTING=1
+        # Arch wiki says so
+        export BEMENU_BACKEND=wayland
+        export MOZ_ENABLE_WAYLAND=1
+    '';
+    wrapperFeatures = {
+      base = true;
+      gtk = true;
+    };
+  };
+  
+  ########################################
+
+  # Enable zsh
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "colored-man-pages" "per-directory-history" ];
+      theme = "terminalparty";
+    };
+  };
+
+  # Enable java jdk and jre
+  programs.java.enable = true;
+
+  # Enable ccache
+  programs.ccache.enable = true;
+
+  # Enable gnupg
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryFlavor = "tty";
+  };
+
+  # "force" disable geary & gnome-terminal
+  # only relevant when gnome de is enabled
+  programs.geary.enable = false;
+  programs.gnome-terminal.enable = false;
+ 
+  ########################################
+
+  # Pin stuff that can break quick
+  nixpkgs.overlays = [
+    # Pinning individual packages that break on unstable.  Note we use an
+    # overlay rather than updating the package in systemPackages so that the
+    # configuration propogates to all options.
+    (self: super: let
+      nixosstable = import <nixos-stable> {
+        config.allowUnfree = true;
+      };
+    in {
+      steam = nixosstable.pkgs.steam;
+      steam-run = nixosstable.pkgs.steam-run;
+      lutris = nixosstable.pkgs.lutris;
+    })
+  ];
+  #########################################
+
+  # Packages that will be installed in system profile.
+  environment = {
+    systemPackages = with pkgs; [
+      wget curl git git-lfs
+      unzip unrar file mkpasswd
+      kakoune most htop
+      ntfs3g
+    ];
+    variables = {
+        EDITOR = "kak";
+        PAGER = "most";
+    };
+  };
+  #########################################
+
+  # Turn off useradd etc. to gain better control
+  users.mutableUsers = false;
+  users.users.yusuf = {
+    isNormalUser = true;
+    home = "/home/yusuf";
+    extraGroups = [ "wheel" "kvm" ];
+    packages = with pkgs; [
+        lutris steam steam-run wineWowPackages.staging # gaym shit
+        tmux neofetch cmus exa # cli shit
+        firefox-wayland ffmpeg mpv python38Packages.youtube-dl-light # net shit
+        rustup godot # just develop things
+        kak-lsp ripgrep bat shellcheck fzf universal-ctags socat gdb perl clang_10 # kide dependency stuff
+        yaru-theme
+        lm_sensors virt-manager
+    ];
+    shell = pkgs.zsh;
+    hashedPassword = "$6$0L/hdI/a$KPa9Hzd/k6Z3PcIuHuseqIcUE1.YjrPk2yUOLL3HdENKt01WxRd9LBadn6P5O6nWXVCH134ur1rvHmfgou7Gl/";
+  };
+  #########################################
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.03"; # Did you read the comment?
+
+}
+
--- README.md
+++ README.md
@@ -1,32 +1,4 @@
-Scripts, configs and some other stuff. Meant to use with `runit` (does not require runit as init, just needs service supervising capabilities).
-
-## Requirements
-- kakoune (<https://github.com/mawww/kakoune>)
-  - kak-lsp (<https://github.com/ul/kak-lsp>)
-  - plug.kak (<https://github.com/andreyorst/plug.kak>)
-- fzf (<https://github.com/junegunn/fzf>)
-- ripgrep (<https://github.com/BurntSushi/ripgrep>)
-- zsh (<https://sourceforge.net/p/zsh/code/ci/master/tree/>)
-  - oh-my-zsh (<https://github.com/ohmyzsh/ohmyzsh>)
-  - fast-syntax-highlighting (<https://github.com/zdharma/fast-syntax-highlighting>)
-- alacritty (<https://github.com/alacritty/alacritty>)
-- rofi (<https://github.com/davatorium/rofi>)
-- mpd (<https://github.com/MusicPlayerDaemon/MPD>)
-  - mpc (<https://github.com/MusicPlayerDaemon/mpc>)
-- ungoogled-chromium (<https://github.com/Eloston/ungoogled-chromium>)
-- Monoid font (<https://larsenwork.com/monoid/>)
-- runit for service supervising (<http://smarden.org/runit/>)
-
-### For wayland
-- sway (<https://github.com/swaywm/sway>)
-  - waybar (<https://github.com/Alexays/Waybar>)
-- grim (<https://github.com/emersion/grim>)
-- imv (<https://github.com/eXeC64/imv>)
-
-### For xorg
-- dwm (<https://dwm.suckless.org/>)
-- feh (<https://github.com/derf/feh>)
-- scrot (<https://github.com/resurrecting-open-source-projects/scrot>)
+NixOS
 
 ## How to setup
 <https://www.atlassian.com/git/tutorials/dotfiles>
