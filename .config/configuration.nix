a56e4ac916d5af4cccb08ce66cefd2db16274df2
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2020-06-18 17:52:21 +0300

    sync on Prş Haz 18 17:52:21 +03 2020

--- .config/configuration.nix
+++ .config/configuration.nix
@@ -1,10 +1,40 @@
 { config, pkgs, ... }:
 
 let
+  # Functions used in config
+  addIf = x: y: if x then y else null;
   dotConf = "git --git-dir=$HOME/.cfg/ --work-tree=$HOME";
+
+  # Personal data
+  userName = "yusuf";
+  userRealName = "Yusuf Bera Ertan";
+  userEmail = "y.bera003.06@protonmail.com";
+
+  # Options
+  enable32Bit = false;
+  
+  useXorg = false; # False means wayland
+  
+  useKakoune = true; # Implies useCliTools
+  useCliTools = useKakoune || true;
+
+  useMediaProd = true;
+  useAudioProd = useMediaProd && true;
+  useVideoProd = useMediaProd && true;
+  useMultimedia = true;
+
+  useNextDns = true;
+  nextDnsId = "75e43d";
+
+  useRust = true;
 in
+
 {
-  nixpkgs.config.allowUnfree = true;
+  nix.trustedUsers = [ "@wheel" ]; # wheel can already edit this file so...
+    
+  nixpkgs.config = {
+    firefox.enablePlasmaBrowserIntegration = true;
+  };
     
   imports =
     [ 
@@ -12,40 +42,50 @@ in
     ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_testing;
-    loader = {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-    };
+    kernelPackages = pkgs.linuxPackages_5_7;
     kernelParams = [ "mitigations=off" ];
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
+    };
     tmpOnTmpfs = true;
   };
 
+  # Set this to true for CPUs with hyperthreading (so anything that's not poor)
   security.allowSimultaneousMultithreading = false;
 
   networking = {
     hostName = "yusuf-pc";
     networkmanager = {
       enable = true;
-      dns = "none"; # We use stubby
+      dns = if useNextDns then "none" else "internal"; # We use stubby
     };
     useDHCP = false;
   };
 
-  i18n.defaultLocale = "tr_TR.UTF-8";
+  i18n = {
+    defaultLocale = "tr_TR.UTF-8";
+  };
+  
   console = {
     font = "7x14";
     keyMap = "trq";
   };
+
   time.timeZone = "Turkey";
 
   sound.enable = true;
   hardware = {
-    pulseaudio.enable = true;
     opengl = {
-      enable = true;
       driSupport = true;
+      driSupport32Bit = enable32Bit;
+      enable = true;
       extraPackages = with pkgs; [ libvdpau-va-gl vaapiVdpau libva vulkan-loader ];
+      extraPackages32 = addIf enable32Bit (with pkgs.pkgsi686Linux; [ libvdpau-va-gl vaapiVdpau libva vulkan-loader ]);
+    };
+    pulseaudio = {
+      enable = true;
+      support32Bit = enable32Bit;
     };
   };
 
@@ -55,38 +95,54 @@ in
   };
 
   environment = {
+    homeBinInPath = true;
     systemPackages = with pkgs; [
       curl git git-lfs
       mkpasswd
-      kakoune htop
+      htop
       ntfs3g
       profile-sync-daemon
       lm_sensors
     ];
-    variables = {
-      EDITOR = "kak";
-    };
     shellAliases = {
-      cat = "bat";
-      config = dotConf;
-      config-sync = "${dotConf} add ~/rember && ${dotConf} commit -a -m \"sync on `date`\" && ${dotConf} push";
-      nixos-conf = "sudo kak /etc/nixos/configuration.nix";
-      nosce = "nixos-conf";
-      ydl = "youtube-dl --embed-thumbnail --extract-audio --audio-format mp3 --add-metadata";
+      cat = "bat"; # The cat died :cry:
       la = "exa --long --grid --git -a";
       ls = "exa";
+      l = "ls";
+      ydl-music = "youtube-dl --embed-thumbnail --extract-audio --audio-format ogg --add-metadata";
+
+      config = dotConf;
+      config-sync = "${dotConf} add ~/rember && ${dotConf} commit -a -m \"sync on `date`\" && ${dotConf} push";
       rember = "kak -e gtd-jump-today ~/rember/stuff`date '+-%m-%Y'`.gtd";
+      
+      nixos-conf = "sudo kak /etc/nixos/configuration.nix";
       nixos-list-generations = "sudo nix-env --list-generations --profile /nix/var/nix/profiles/system";
       nosrs = "sudo nixos-rebuild switch";
       nosrb = "sudo nixos-rebuild boot";
       noslg = "nixos-list-generations";
+      nosce = "nixos-conf";
+
       tmux = "tmux new-session -d -s \>_ 2>/dev/null; tmux new-session -t \>_ \; set-option destroy-unattached";
     };
+    variables = {
+      EDITOR = "kak";
+    };
+  };
+
+  services.xserver = {
+    desktopManager.plasma5.enable = useXorg;
+    displayManager.sddm = {
+        autoLogin = { enable = true; user = "yusuf"; };
+        enable = useXorg;
+    };
+    enable = useXorg;
+    layout = "tr";
+    videoDrivers = [ "amdgpu" ];
   };
 
   programs = {
     sway = {
-      enable = true;
+      enable = !useXorg;
       extraPackages = with pkgs; [
         swaylock swayidle xwayland rofi wl-clipboard grim imv
       ];
@@ -116,7 +172,7 @@ in
       '';
       loginShellInit = ''
        	  if [ "$(tty)" = "/dev/tty1" ]; then
-              exec sway
+              exec ${if useXorg then "startplasma-x11" else "sway"}
           fi
       '';
     };
@@ -127,55 +183,57 @@ in
       keyMode = "vi";
       newSession = true;
       shortcut = "a";
-      extraConfig = "
+      extraConfig = ''
     	  set -g default-terminal 'xterm-256color'
     	  set -ga terminal-overrides ',*256col*:Tc'
     	  set -g status off
-      ";
+      '';
     };
     gnupg.agent = {
       enable = true;
       enableSSHSupport = true;
-      pinentryFlavor = "tty";
+      pinentryFlavor = if useXorg then "qt" else "tty";
     };
   };
 
   services = {
     stubby = {
-      enable = true;
+      enable = useNextDns;
       roundRobinUpstreams = false;
       upstreamServers = ''
           - address_data: 45.90.28.0
-            tls_auth_name: "75e43d.dns1.nextdns.io"
+            tls_auth_name: "${nextDnsId}.dns1.nextdns.io"
           - address_data: 2a07:a8c0::0
-            tls_auth_name: "75e43d.dns1.nextdns.io"
+            tls_auth_name: "${nextDnsId}.dns1.nextdns.io"
           - address_data: 45.90.30.0
-            tls_auth_name: "75e43d.dns2.nextdns.io"
+            tls_auth_name: "${nextDnsId}.dns2.nextdns.io"
           - address_data: 2a07:a8c1::0
-            tls_auth_name: "75e43d.dns2.nextdns.io"
+            tls_auth_name: "${nextDnsId}.dns2.nextdns.io"
       '';
     };
     psd.enable = true;
   };
 
-  users = {
+  users = with pkgs; {
     mutableUsers = false;
     users.yusuf = {
       isNormalUser = true;
       home = "/home/yusuf";
       extraGroups = [ "wheel" ];
-      packages = with pkgs; [
-        alacritty
-        exa neofetch
-        firefox-wayland spectral
-        musikcube ffmpeg mpv python38Packages.youtube-dl-light playerctl lmms krita
-        kak-lsp ripgrep bat universal-ctags
-        clang_10 llvmPackages.bintools
-    	# nixFlakes
-    	rust-analyzer rustc cargo clippy rustfmt cargo-watch hyperfine
-    	godot
-      ];
-      shell = pkgs.zsh;
+      packages =
+        addIf useAudioProd [lmms audacity] ++
+        addIf useCliTools [alacritty exa neofetch hyperfine ripgrep bat] ++
+        [(firefox.overrideAttrs (oldAttrs: rec {
+          enableOfficialBranding = false;
+          gssSupport = false;
+          privacySupport = true;
+        }))] ++
+        addIf useKakoune [kakoune kak-lsp universal-ctags] ++
+        addIf useMediaProd [ffmpeg youtube-dl] ++
+        addIf useMultimedia [mpv musikcube playerctl] ++
+    	addIf useRust [rust-analyzer rustc cargo clippy rustfmt cargo-watch] ++
+        addIf useVideoProd [kdenlive];
+      shell = zsh;
       hashedPassword = "$6$0L/hdI/a$KPa9Hzd/k6Z3PcIuHuseqIcUE1.YjrPk2yUOLL3HdENKt01WxRd9LBadn6P5O6nWXVCH134ur1rvHmfgou7Gl/";
     };
   };
